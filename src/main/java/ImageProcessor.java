20233eed3087a5737f3f188a90f8740e78fedd61
Mike Lynch <mjlynch2@gmail.com>
2020-01-30 08:43:28 -0600

    initial commit

---
+++ src/main/java/ImageProcessor.java
@@ -0,0 +1,24 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+public class ImageProcessor {
+
+    private static ImageFilter filter;
+    private static BufferedImage image;
+
+    public static void main(String[] args) {
+        image = null;
+        try {
+            URL imageUrl = new URL("https://pixabay.com/get/50e4d246485ab108f5d08460962933771636d9e0564c704c722679d49349c150_640.jpg");
+            image = ImageIO.read(imageUrl.openStream());
+        }
+        catch (IOException e) {
+            System.out.println(e);
+    }
+        System.out.println(image);
+    }
+}
---
+++ src/main/java/Sepia.java
@@ -0,0 +1,35 @@
+import java.awt.image.BufferedImage;
+
+public class Sepia implements ImageFilter {
+    public BufferedImage filter(BufferedImage image){
+        BufferedImage result = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        for(int y = 0; y < image.getHeight(); y++){
+            for(int x = 0; x < image.getWidth(); x++) {
+                int rgb = image.getRGB(x, y);
+
+                int red = (rgb >> 16) & 0xff;
+                int green = (rgb >> 8) & 0xff;
+                int blue = rgb & 0xff;
+
+                double redNew = (red * 0.393) + (green * 0.769) + (blue * 0.189);
+                double greenNew = (red * .349) + (green * .686) + (blue * .168);
+                double blueNew = (red * .272) + (green * .534) + (blue * .131);
+
+                if (redNew > 255) {
+                    redNew = 255;
+                }
+                if (greenNew > 255) {
+                    greenNew = 255;
+                }
+                if (blueNew > 255) {
+                    blueNew = 255;
+                }
+
+                int newRGB = ((int) redNew << 16) | ((int) greenNew << 8) | (int) blueNew;
+                result.setRGB(x, y, newRGB);
+            }
+        }
+        return result;
+    }
+}
